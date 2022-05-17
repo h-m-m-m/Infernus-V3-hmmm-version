25bcfaef4b28d9db15d3a07ce09d312f50bf8d5c
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-17 19:28:17 +0100

    Update Manager.h

--- Client/Infernus/Manager/Manager.h
+++ Client/Infernus/Manager/Manager.h
@@ -3,15 +3,18 @@
 
 #include "../../Client.h"
 #include "../Hook/Hook.h"
+#include "../Category/Category.h"
 
 class Manager {
 public:
     Client* client = nullptr;
     std::vector<Hook*> hooks;
+    std::vector<Category*> categories;
 public:
     Manager(Client*);
 public:
     auto initHooks(void) -> void;
+    auto initCategories(void) -> void;
 };
 
 #endif /* CLIENT_INFERNUS_MANAGER_MANAGER */
