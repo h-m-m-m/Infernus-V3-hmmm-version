1dddccf9cec778845455ee2de7762f97e8512dca
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-17 22:24:25 +0100

    Update Minecraft.cpp

--- Client/SDK/Minecraft.cpp
+++ Client/SDK/Minecraft.cpp
@@ -1,5 +1,13 @@
 #include "Minecraft.h"
 
 auto Minecraft::getClientInstance(void) -> ClientInstance* {
-    return (ClientInstance*)Mem::findMultiLvlPtr(0x05473C38, { 0x0, 0x48, 0x0 });
+    static auto sig = (uintptr_t)NULL;
+
+    if(!sig)
+        sig = Mem::findSig("48 8B 15 ? ? ? ? 4C 8B 02 4C 89 06 40 84 FF ? ? 48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8B C6 48 8B 4C 24 ? 48 33 CC E8 ? ? ? ? 48 8B 5C 24 ? 48 8B 74 24 ? 48 83 C4 ? 5F C3 E8 ? ? ? ? 90 CC CC CC CC CC CC CC CC CC 48 89 5C 24 ? 48 89 6C 24");
+    
+    auto offset = *reinterpret_cast<int*>(sig + 3);
+    auto basePtr = (uintptr_t)(sig + offset + 7) - Mem::getGameModule();
+
+    return (ClientInstance*)Mem::findMultiLvlPtr(basePtr, { 0x0, 0x48, 0x0 });
 };
