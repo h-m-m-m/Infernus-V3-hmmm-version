328fab13914cb8d641918a32d42d0e117c909692
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-22 02:13:47 +0100

    Update Module.cpp

--- Client/Infernus/Module/Module.cpp
+++ Client/Infernus/Module/Module.cpp
@@ -7,3 +7,17 @@ Module::Module(Category* category, std::string name) {
 
     this->category->modules.push_back(this);
 };
+
+auto Module::baseTick(void) -> void {
+    if(this->wasEnabled != this->isEnabled) {
+        this->wasEnabled = this->isEnabled;
+
+        if(this->isEnabled)
+            this->onEnable();
+        else
+            this->onDisable();
+    };
+
+    if(this->isEnabled)
+        this->onTick();
+};
