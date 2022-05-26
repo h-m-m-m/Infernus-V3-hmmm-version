ba32a27db63ddfce30bcd59ef3f3bf73af8c75f8
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-26 20:33:14 +0100

    Create ClickGui.h

---
+++ Client/Infernus/Module/Modules/Visuals/ClickGui.h
@@ -0,0 +1,16 @@
+#ifndef CLIENT_INFERNUS_MODULE_MODULES_VISUALS_CLICKGUI
+#define CLIENT_INFERNUS_MODULE_MODULES_VISUALS_CLICKGUI
+
+#include "../../Module.h"
+
+class ClickGui : public Module {
+public:
+    ClickGui(Category* c) : Module(c, "Click Gui") {
+        this->setKey(VK_INSERT);
+        this->setState(true);
+    };
+public:
+    virtual auto onRender(void) -> void override;
+};
+
+#endif /* CLIENT_INFERNUS_MODULE_MODULES_VISUALS_CLICKGUI */
