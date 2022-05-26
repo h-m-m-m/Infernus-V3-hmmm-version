58f1dbe9f7c4bc1b070a8d4d74573c1e23089675
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-26 11:43:55 +0100

    Update Actor.cpp

--- Client/SDK/Classes/Actor.cpp
+++ Client/SDK/Classes/Actor.cpp
@@ -1,5 +1,16 @@
 #include "Actor.h"
+#include "../Minecraft.h"
 
 auto Actor::getLevel(void) -> Level* {
-    return *(Level**)((uintptr_t)(this) + 0x368);
+    static auto result = (Level*)nullptr;
+
+    if(result == nullptr) {
+        switch(Minecraft::getVersion().second) {
+            case MC_VER::v1_18_31:
+                result = *(Level**)((uintptr_t)(this) + 0x368);
+            break;
+        };
+    };
+
+    return result;
 };
