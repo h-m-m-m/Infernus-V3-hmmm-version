36e861b956e87ff515b086f1778de56aa404bb7b
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-26 11:43:53 +0100

    Update Player.cpp

--- Client/SDK/Classes/Player.cpp
+++ Client/SDK/Classes/Player.cpp
@@ -1,11 +1,17 @@
 #include "Player.h"
+#include "../Minecraft.h"
 
 auto Player::setPlayerGameType(int type) -> void {
     using SetPlayerGameType = void (__thiscall*)(Player*, int);
     static auto _SetPlayerGameType = (SetPlayerGameType)nullptr;
 
-    if(_SetPlayerGameType == nullptr)
-        _SetPlayerGameType = (SetPlayerGameType)Mem::findSig("48 89 5C 24 10 48 89 6C 24 18 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 8B");
+    if(_SetPlayerGameType == nullptr) {
+        switch(Minecraft::getVersion().second) {
+            case MC_VER::v1_18_31:
+                _SetPlayerGameType = (SetPlayerGameType)Mem::findSig("48 89 5C 24 10 48 89 6C 24 18 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 8B");
+            break;
+        };
+    };
     
     if(_SetPlayerGameType != nullptr)
         _SetPlayerGameType(this, type);
