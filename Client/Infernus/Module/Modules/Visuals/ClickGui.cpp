4b3546c6399c479af69d0ec0bbadf84707aa4249
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-26 20:33:18 +0100

    Create ClickGui.cpp

---
+++ Client/Infernus/Module/Modules/Visuals/ClickGui.cpp
@@ -0,0 +1,50 @@
+#include "ClickGui.h"
+
+#include "../../../Category/Category.h"
+#include "../../../Manager/Manager.h"
+
+auto ClickGui::onRender(void) -> void {
+    
+    auto mgr = this->category->manager;
+    
+    if(ImGui::Begin(this->name.c_str())) {
+
+        for(auto category : mgr->categories) {
+            
+            if(ImGui::BeginMenu(category->name.c_str())) {
+
+                for(auto mod : category->modules) {
+
+                    if(ImGui::BeginMenu(mod->name.c_str())) {
+
+                        if(ImGui::MenuItem(mod->isEnabled ? "Disable" : "Enable")) {
+
+                            mod->isEnabled = !mod->isEnabled;
+
+                        };
+
+                        if(ImGui::BeginMenu("Options")) {
+
+                            
+                            mod->onRenderOptions();
+                            ImGui::EndMenu();
+
+                        };
+                        
+                        ImGui::EndMenu();
+
+                    };
+
+                };
+
+                ImGui::EndMenu();
+
+            };
+
+        };
+
+        ImGui::End();
+
+    };
+
+};
