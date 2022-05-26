9ebe93304264a5d04f37d5f866fea6720c710e71
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-26 11:43:51 +0100

    Update Level.cpp

--- Client/SDK/Classes/Level.cpp
+++ Client/SDK/Classes/Level.cpp
@@ -1,8 +1,17 @@
 #include "Level.h"
+#include "../Minecraft.h"
 
 auto Level::getPrimaryLocalPlayer(void) -> Actor* {
     using GetPrimaryLocalPlayer = Actor* (__thiscall*)(Level*);
-    auto _GetPrimaryLocalPlayer = (GetPrimaryLocalPlayer)(this->VTable[59]);
+    auto _GetPrimaryLocalPlayer = (GetPrimaryLocalPlayer)nullptr;
 
-    return _GetPrimaryLocalPlayer(this);
+    if(_GetPrimaryLocalPlayer == nullptr) {
+        switch(Minecraft::getVersion().second) {
+            case MC_VER::v1_18_31:
+                _GetPrimaryLocalPlayer = (GetPrimaryLocalPlayer)(this->VTable[59]);
+            break;
+        };
+    };
+
+    return (_GetPrimaryLocalPlayer != nullptr ? _GetPrimaryLocalPlayer(this) : nullptr);
 };
