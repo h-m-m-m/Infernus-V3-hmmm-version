3f4f6ab7aaffc98e862db9e998c64c9c0255cf9f
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-27 06:15:45 +0100

    Update Killaura.cpp

--- Client/Infernus/Module/Modules/Combat/Killaura.cpp
+++ Client/Infernus/Module/Modules/Combat/Killaura.cpp
@@ -9,9 +9,11 @@ auto Killaura::onGameMode(GameMode* GM) -> void {
 
     
     auto mgr = this->category->manager;
+    auto distances = std::vector<float>();
+
     for(auto [runtimeId, entity] : mgr->entityMap) {
 
-        if(runtimeId == player->getRuntimeId())
+        if(runtimeId == player->getRuntimeId() || !entity->isAlive() || !player->canAttack(entity, false))
             continue;
         
         auto entPos = entity->getPos();
@@ -22,9 +24,32 @@ auto Killaura::onGameMode(GameMode* GM) -> void {
 
         auto dist = sqrt(dX * dX + dY * dY + dZ * dZ);
 
-        if(dist <= 12.f) {
+        if(dist <= 12.f)
+            distances.push_back(dist);
+        
+    };
+
+    std::sort(distances.begin(), distances.end());
+
+    if(distances.empty())
+        return;
+    
+    for(auto [runtimeId, entity] : mgr->entityMap) {
+
+        if(runtimeId == player->getRuntimeId() || !entity->isAlive() || !player->canAttack(entity, false))
+            continue;
+        
+        auto entPos = entity->getPos();
+
+        auto dX = myPos.x - entPos.x;
+        auto dY = myPos.y - entPos.y;
+        auto dZ = myPos.z - entPos.z;
+
+        auto dist = sqrt(dX * dX + dY * dY + dZ * dZ);
+
+        if(dist == distances[0] || (distances.size() > 1 ? dist == distances[0] : dist == distances[1]) || (distances.size() > 2 ? dist == distances[1] : dist == distances[2])) {
             GM->attack(entity);
         };
-
+        
     };
 };
