19c1da2c897c3fd552aeb2e56d89488cec1368fb
EchoHackCmd <EchoHackCmd@gmail.com>
2022-05-29 14:11:38 +0100

    Create AirJump.h

---
+++ Client/Infernus/Module/Modules/Movement/AirJump.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_INFERNUS_MODULE_MODULES_MOVEMENT_AIRJUMP
+#define CLIENT_INFERNUS_MODULE_MODULES_MOVEMENT_AIRJUMP
+
+#include "../../Module.h"
+
+class AirJump : public Module {
+public:
+    AirJump(Category* c) : Module(c, "AirJump") {
+        //
+    };
+public:
+    virtual auto onGameMode(GameMode*) -> void override;
+};
+
+#endif /* CLIENT_INFERNUS_MODULE_MODULES_MOVEMENT_AIRJUMP */
